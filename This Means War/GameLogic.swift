f16960acb058b816304bc040edf800f9545506e5
topcat15 <danny@foundryinteractive.com>
2021-01-28 02:04:14 -0800

    Added several properties to GameLogic. I now differentiate between a player's deck, a draw pile, and a heal pile. A player's deck is the combo of their draw and heal piles. The heal piles is where cards go after battle. They remain in the heal pile until the draw pile is empty, at which time the heal pile is shuffled, set to the draw pile, and cleared.
    moveToBattleField() has been refined, and considers how many cards are already present on the battlefield, which determines the faceUp value of a card. This func also calls the checkDrawIsEmpty function after every time a card is moved from a draw pile.
    
    A battle() func encapsulates the win/tie scenarios and the movement of cards between the piles, which inherently tracks the players' scores.
    
    A checkDrawIsEmpty() func will make sure that the draw pile always has cards in it.

--- This Means War/GameLogic.swift
+++ This Means War/GameLogic.swift
@@ -13,6 +13,15 @@ class GameLogic {
     public var player1Score:Int
     public var player2Score:Int
     private var atWar = false
+    // Each player's deck consists of a draw pile and a heal pile
+    var player1Deck = [Card]()
+    var player2Deck = [Card]()
+    var drawPilePlayer1 = [Card]()
+    var drawPilePlayer2 = [Card]()
+    var healPilePlayer1 = [Card]()
+    var healPilePlayer2 = [Card]()
+    var drawIsEmptyPlayer1 = false
+    var drawIsEmptyPlayer2 = false
     
     init() {
         
@@ -20,79 +29,141 @@ class GameLogic {
         // Player score = number of cards in player deck
         self.player1Score = deck!.player1Deck.count
         self.player2Score = deck!.player2Deck.count
+        self.drawPilePlayer1 = deck!.player1Deck
+        self.drawPilePlayer2 = deck!.player2Deck
+        self.player1Deck = drawPilePlayer1 + healPilePlayer1
+        self.player2Deck = drawPilePlayer2 + healPilePlayer2
+        self.checkDrawIsEmpty()
         
     }
-
-    // The Battle Field
     
-    var battlePosition1:Card?
-    var battlePosition2:Card?
+    // The Battle Field
+    var battlePosition1 = [Card]()
+    var battlePosition2 = [Card]()
     
     // Place the top card from players' decks into the corresponding battle positions
     func moveToBattleField() {
         
-        for (index, card) in deck!.player1Deck.enumerated() {
-            
-            if index == 0 {
+        battlePosition1.append(drawPilePlayer1.first!)
+        battlePosition2.append(drawPilePlayer2.first!)
+         
+        for (index, _) in battlePosition1.enumerated() {
+        
+            // Flip over any card in a 0- or even-indexed position in the battlePosition arrays
+            if index % 2 == 0 {
                 
-                battlePosition1 = card
-                card.flip()
+                battlePosition1[index].faceUp = true
+                battlePosition2[index].faceUp = true
                 
             }
+            // Do not flip over any card in an odd-indexed position in the battlePosition arrays
             else {
-                return
+                battlePosition1[index].faceUp = false
+                battlePosition2[index].faceUp = false
             }
-            
-            // Remove card from player's deck
-            deck!.player1Deck.remove(at: 0)
         }
         
-        for (index, card) in deck!.player1Deck.enumerated() {
+        // Remove the cards from the draw pile arrays
+        drawPilePlayer1.removeFirst()
+        drawPilePlayer2.removeFirst()
+        
+        // Every time we move a card onto the battle field, check if the draw piles are empty afterward
+        checkDrawIsEmpty()
+
+    }
+    
+    // Battle - determine who wins
+    func battle() {
+        
+        repeat {
             
-            if index == 0 {
+            // First, check to see if we are at war
+            if battlePosition1.count > 1 {
                 
-                battlePosition2 = card
-                card.flip()
+                moveToBattleField()
+                moveToBattleField()
                 
             }
-            else {
-                return
+            
+            // Second, check to make sure Battle Field is not empty (otherwise you cannot battle)
+            else if battlePosition1.count > 0 && battlePosition2.count > 0 {
+                
+                moveToBattleField()
+                
+                if battlePosition1.last!.value > battlePosition2.last!.value {
+                
+                    // Add cards to player 1's heal pile
+                    healPilePlayer1.append(contentsOf: battlePosition1)
+                    healPilePlayer1.append(contentsOf: battlePosition2)
+                    
+                    battlePosition1.removeAll()
+                    battlePosition1.removeAll()
+                
+                }
+                else if battlePosition1.last!.value < battlePosition2.last!.value {
+                    
+                    // Add cards to player 2's heal pile
+                    healPilePlayer2.append(contentsOf: battlePosition1)
+                    healPilePlayer2.append(contentsOf: battlePosition2)
+                    
+                    battlePosition1.removeAll()
+                    battlePosition1.removeAll()
+                    
+                }
+                
             }
             
-            deck!.player2Deck.remove(at: 0)
+        } while battlePosition1.last!.value == battlePosition2.last!.value
+    }
+
+    // When a draw pile is empty, the heal pile is shuffled and then transferred to the draw pile
+    func checkDrawIsEmpty() {
+        
+        let drawCountPlayer1 = drawPilePlayer1.count
+        let drawCountPlayer2 = drawPilePlayer2.count
+        
+        // For player 1
+        switch drawCountPlayer1 {
+        
+        case 0:
+            
+            drawIsEmpty = true
+            drawPilePlayer1.append(contentsOf: healPilePlayer1.shuffled())
+            healPilePlayer1.removeAll()
+        
+        default:
+            
+            drawIsEmpty = false
+        
         }
         
+        // For player 2
+        switch drawCountPlayer2 {
+        
+        case 0:
+            
+            drawIsEmpty = true
+            drawPilePlayer2.append(contentsOf: healPilePlayer2.shuffled())
+            healPilePlayer2.removeAll()
+        
+        default:
+            
+            drawIsEmpty = false
+        
+        }
         
     }
     
-    
-
-
-    
-    // Who wins is decided by whose battle card has a higher value
-    
-        // If a player wins a normal round, their score increases by one, as they have a gained a card from the opposing player
-    
-    // If the battle cards have the same value, then we enter the war state
-    
-        // WAR
+    func checkWinner {
         
-            // Each player places one card face down and another face up
-        
-            // If the two face up cards have different values, the one with the higher value wins
-        
-                // The winner's score is increased by 2 * the number of rounds of war
-        
-            // War ends when  depends on the value of the face up card
-            
-                // If the values match again, then a second round of War commences
-        
+        if player1deck.count = 0 {
+            print("PLAYER 2 WINS!!!!!")
+        }
+        else {
+            print("PLAYER 1 WINS!!!!!")
+        }
+    }
                 
-        
-            // Whoever wins will have t
-    
-    
-    
     
     
 //    func deal() -> [Card]? {
@@ -102,7 +173,6 @@ class GameLogic {
 //
 //            // determine who wins
 //
-//            // calc score update player1Score, player2Score before return
 //
 //        }
 //        else {
@@ -131,32 +201,19 @@ class GameLogic {
 }
 
 
-
-    // The top card from each player's deck is automatically moved into the Battle Position, with the back of the card being visible
-
-
-    // The back of the card is hiidden from view thereafter
-
-
 // Draw Pile
 
     // No cards appear in discard pile when it is empty
 
     // OPTIONAL: show outline on board where discard pile goes
 
-// Discard Pile
+// Heal Pile
 
     // No cards appear in discard pile when it is empty
 
     // OPTIONAL: show outline on board where discard pile goes
 
 
-// Determine who wins round
-
-// TODO: When a player wins, they inherit the card from that battle, which is placed in their discard pile
-
 // TODO: When a player's draw pile is empty, shuffle their discard pile and make it their deck
-
-// TODO: Tie condition - players draw cards until one of them wins, and winner claims all cards
 
 // TODO: Win condition - a player wins when the other player has no more cards to draw
