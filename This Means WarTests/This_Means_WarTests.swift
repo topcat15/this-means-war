8dd5be0652d1820e74c32e29151361bc3f1ebd8b
topcat15 <danny@foundryinteractive.com>
2021-02-09 10:53:36 -0800

    Added the unit test template file. Haven't added any tests yet though.

---
+++ This Means WarTests/This_Means_WarTests.swift
@@ -0,0 +1,37 @@
+//
+//  This_Means_WarTests.swift
+//  This Means WarTests
+//
+//  Created by Daniel J Brown on 2/6/21.
+//
+
+import XCTest
+
+class This_Means_WarTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testGameCompletesWithoutError() throws {
+        
+        
+    }
+
+}
