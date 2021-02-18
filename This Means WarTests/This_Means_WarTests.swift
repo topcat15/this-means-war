a1c1a1c6a699f3a0eb5497eaa5e36eeb7abed27e
topcat15 <danny@foundryinteractive.com>
2021-02-18 10:25:09 -0800

    First attempt at writing unit tests for This Means War.

--- This Means WarTests/This_Means_WarTests.swift
+++ This Means WarTests/This_Means_WarTests.swift
@@ -6,32 +6,45 @@
 //
 
 import XCTest
+@testable import This_Means_War
 
 class This_Means_WarTests: XCTestCase {
 
+    var game: WarGameLogic!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        game = WarGameLogic()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        game = nil
     }
+    
+    // MARK: - Player Deck Tests
+     
+    // This a
+    func testDefaultImageNameForCardsAtGameStart() {
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        let expectedArray1 = Array(repeating: false, count: 26)
+        let expectedArray2 = Array(repeating: "back", count: 26)
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+        XCTAssertEqual(game.firstPlayerDeck.map{$0.faceUp}, expectedArray1)
+        XCTAssertEqual(game.secondPlayerDeck.map{$0.faceUp}, expectedArray1)
+//
+
+        func testGameCompletesWithoutError() throws {
+
+            XCTAssertEqual(game.firstPlayerDeck.map{$0.imageName}, expectedArray2)
+            XCTAssertEqual(game.secondPlayerDeck.map{$0.imageName}, expectedArray2)
         }
     }
     
-    func testGameCompletesWithoutError() throws {
-        
-        
-    }
-
+    // TODO: Write tests for number of cards in deck and player decks
+    
+    // TODO: To test that all the expected values are in the deck, I could check that there are 52 cards and that each card in the deck is unique
+    
+    // TODO: To test randomization of the dealt deck, I could check that the dealt deck does not equal the initial deck or 2)
+    
 }
